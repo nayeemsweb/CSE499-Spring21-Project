5756154f13d9573e7c4bb016ec5270628135cd1f
Sadman25 <shadman.saqif@northsouth.edu>
2021-03-29 22:22:39 +0600

    Accounts app created, not migrated yet #4

---
+++ Code/Backend/classroom/accounts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from PIL import Image
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=True)
+
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile') 
+    image = models.ImageField(default='default.jpg',upload_to='student_pics')
+    studentID = models.CharField(max_length=20,blank=False)
+
+
+class FacultyProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='faculty_profile') 
+    image = models.ImageField(default='default.jpg',upload_to='faculty_pics')
+    facultyID = models.CharField(max_length=20,blank=False)
