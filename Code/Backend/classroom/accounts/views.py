346d97064e347c83b4a0e9c8d53f209561690dbc
zsabit <sabit.zubair@northsouth.edu>
2021-03-30 18:23:06 +0600

    Routing #7: fixed sign_up template loading

--- Code/Backend/classroom/accounts/views.py
+++ Code/Backend/classroom/accounts/views.py
@@ -4,5 +4,5 @@ from django.contrib import messages
 # Create your views here.
 
 def student_profile_signup(request):
-    return render(request, '../templates/sign_up.html')
+    return render(request, 'sign_up.html')
         
