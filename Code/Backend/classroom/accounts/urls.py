64fe87c8dd761adbcabc2c8cb096c274e48bfc77
moktadirkhan <moktadirkhan@gmail.com>
2021-04-05 01:50:50 +0600

    commit

--- Code/Backend/classroom/accounts/urls.py
+++ Code/Backend/classroom/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.shortcuts import render
+from django.http import HttpResponse
 from .import views
 
 urlpatterns =[
     path('', views.register, name='register'),
     path('studentRegister/', views.studentRegister.as_view(), name='studentRegister'),
     path('facultyRegister/', views.facultyRegister.as_view(), name='facultyRegister'),
+    path('live/', views.live),
 ]
+
--- Code/Backend/classroom/accounts/views.py
+++ Code/Backend/classroom/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render , redirect
 from .models import User, StudentProfile, FacultyProfile
 from .forms import StudentProfileForm, FacultyProfileForm
 from django.views.generic import CreateView
+from django.http import HttpResponse
 # Create your views here.
 
 def register(request):
@@ -17,3 +18,6 @@ class facultyRegister(CreateView):
     model = User
     form_class = FacultyProfileForm
     template_name = '../templates/sign_up.html'
+
+def live(request):
+     return render(request,'../templates/live_streams.html') 
