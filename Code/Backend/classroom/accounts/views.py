35635878632dd5e0e9bb99a592bbd7803a604988
Sadman25 <shadman.saqif@northsouth.edu>
2021-04-22 15:59:12 +0600

    created new file to handle decorators #18

--- Code/Backend/classroom/accounts/views.py
+++ Code/Backend/classroom/accounts/views.py
@@ -6,75 +6,69 @@ from .forms import StudentForm, FacultyForm, UserRegistration
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .decorators import *
 
-
+@unauthenticated_user
 def FacultyRegistration(request):
-    if request.user.is_authenticated:
-        return redirect('homePage')
+    userform = UserRegistration()
+    facultyform = FacultyForm()
+
+    if request.method == 'POST':
+        userform = UserRegistration(request.POST)
+        facultyform = FacultyForm(request.POST)
+        if userform.is_valid() and facultyform.is_valid():
+            user = userform.save()            
+            user.save()
+            faculty = facultyform.save(commit=False)
+            faculty.user = user
+            faculty.save()
+            messages.success(request,'Account was created for ' + faculty.user.username)
+            return redirect ('loginPage')
     else:
+        print (userform.errors, facultyform.errors)
 
-        userform = UserRegistration()
-        facultyform = FacultyForm()
+    context = {'facultyform': facultyform, 'userform': userform} 
+    return render(request, 'faculty.html', context)
 
-        if request.method == 'POST':
-            userform = UserRegistration(request.POST)
-            facultyform = FacultyForm(request.POST)
-            if userform.is_valid() and facultyform.is_valid():
-                user = userform.save()            
-                user.save()
-                faculty = facultyform.save(commit=False)
-                faculty.user = user
-                faculty.save()
-                messages.success(request,'Account was created for ' + faculty.user.username)
-                return redirect ('loginPage')
-        else:
-            print (userform.errors, facultyform.errors)
+@unauthenticated_user
+def StudentRegistration(request):    
+    userform = UserRegistration()
+    studentform = StudentForm()
 
-        context = {'facultyform': facultyform, 'userform': userform} 
-        return render(request, 'faculty.html', context)
-
-
-def StudentRegistration(request):
-    if request.user.is_authenticated:
-        return redirect('homePage')
+    if request.method == 'POST':
+        userform = UserRegistration(request.POST)
+        studentform = StudentForm(request.POST)
+        if userform.is_valid() and studentform.is_valid():
+            user = userform.save()
+            user.save()
+            student = studentform.save(commit=False)
+            student.user = user
+            student.save()
+            messages.success(request,'Account was created for ' + student.user.username)
+            return redirect ('loginPage')
     else:
-        userform = UserRegistration()
-        studentform = StudentForm()
+        print (userform.errors, studentform.errors)
 
-        if request.method == 'POST':
-            userform = UserRegistration(request.POST)
-            studentform = StudentForm(request.POST)
-            if userform.is_valid() and studentform.is_valid():
-                user = userform.save()
-                user.save()
-                student = studentform.save(commit=False)
-                student.user = user
-                student.save()
-                messages.success(request,'Account was created for ' + student.user.username)
-                return redirect ('loginPage')
-        else:
-            print (userform.errors, studentform.errors)
+    context = {'userform': userform, 'studentform': studentform}
+    return render(request, 'student.html', context)
 
-        context = {'userform': userform, 'studentform': studentform}
-        return render(request, 'student.html', context)
 
+@unauthenticated_user
 def loginPage(request):
-    if request.user.is_authenticated:
-        return redirect ('homePage')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
 
-            user = authenticate(request, username = username, password = password)
-            if user is not None:
-                login(request, user)
-                return redirect ('homePage')
-            else:
-                messages.info (request, 'Email address or Password is incorrect')
-                #return render(request, 'sign_in.html')
-        context = {}
-        return render(request, 'sign_in.html', context)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect ('homePage')
+        else:
+            messages.info (request, 'Email address or Password is incorrect')
+            #return render(request, 'sign_in.html')
+    context = {}
+    return render(request, 'sign_in.html', context)
 
 @login_required(login_url='loginPage')
 def homePage(request):
