ead934172455cc77dda85893ed2b1f2191ad38cc
Sadman25 <shadman.saqif@northsouth.edu>
2021-05-01 16:38:19 +0600

    Added imagefield and manually retrieved from DB so far #19

--- Code/Backend/classroom/accounts/models.py
+++ Code/Backend/classroom/accounts/models.py
@@ -10,6 +10,7 @@ class Student (models.Model):
     user = models.OneToOneField(User, null=True, blank= True, on_delete= models.CASCADE)
     studentID = models.CharField(max_length=20, null=True)
     department = models.ForeignKey(Department, null=True, on_delete= models.SET_NULL)
+    profile_pic = models.ImageField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.user.username
@@ -19,6 +20,7 @@ class Faculty (models.Model):
     user = models.OneToOneField(User, null=True, blank= True, on_delete= models.CASCADE)
     facultyID = models.CharField(max_length=20)
     bio = models.CharField(max_length=500)
+    profile_pic = models.ImageField(null=True, blank=True)
     department = models.ForeignKey(Department, null=True, on_delete= models.SET_NULL)
 
 
--- Code/Backend/classroom/classroom/settings.py
+++ Code/Backend/classroom/classroom/settings.py
@@ -128,5 +128,5 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL ='/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_URL ='/images/' 
