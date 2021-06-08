bb4e88e8227f2f50cb7cf1c4a7ea7bd4d877db23
Sadman25 <shadman.saqif@northsouth.edu>
2021-06-08 20:57:47 +0600

    #20 Updated the database

--- Code/Backend/classroom/classes/admin.py
+++ Code/Backend/classroom/classes/admin.py
@@ -1,7 +1,7 @@
-from classes.models import Classroom, Comments, Post
+from classes.models import Classroom, Comment, Post
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(Classroom)
 admin.site.register(Post)
-admin.site.register(Comments)
+admin.site.register(Comment)
--- Code/Backend/classroom/classes/models.py
+++ Code/Backend/classroom/classes/models.py
@@ -1,28 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 from accounts.models import Faculty
-
+import uuid
 class Classroom (models.Model):
     facultyiD=models.OneToOneField(Faculty, on_delete= models.CASCADE)
-    class_code=models.CharField(max_length=20)
+    class_code=models.UUIDField(
+         primary_key = True,
+         default = uuid.uuid4,
+         editable = False)
     class_pics=models.ImageField(null=True, blank=True)
     course_title=models.CharField(max_length=20)
     course_subtitle=models.CharField(max_length=20,null=True)
     course_description=models.CharField(max_length=500,null=True)
-    fb_link=models.CharField(max_length=200,null=True)
-    discord_link=models.CharField(max_length=200,null=True)
-
+    
 
 class Post (models.Model):
-    class_code=models.CharField(max_length=20)
+    class_code=models.ForeignKey(Classroom,on_delete=models.CASCADE)
     post=models.CharField(max_length=1000)
-    like_btn=models.BooleanField()
-    userID=models.CharField(max_length=20)
+    userID=models.ForeignKey(User,on_delete=models.CASCADE)
 
-class Comments(models.Model):
+class Comment(models.Model):
     postID=models.ForeignKey(Post, on_delete= models.CASCADE)
     comments=models.CharField(max_length=1000)
-    userID=models.CharField(max_length=20)
+    userID=models.ForeignKey(User,on_delete=models.CASCADE)
 
 
 
