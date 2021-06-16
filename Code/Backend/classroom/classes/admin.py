79c421154673fc7ef1b8bdcd674b9ba3c3f54743
Sadman25 <shadman.saqif@northsouth.edu>
2021-06-17 05:10:22 +0600

    #20 Updated the model

--- Code/Backend/classroom/classes/admin.py
+++ Code/Backend/classroom/classes/admin.py
@@ -1,7 +1,8 @@
-from classes.models import Classroom, Comment, Post
+from classes.models import ClassTime,Classroom, Comment, Post
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(ClassTime)
 admin.site.register(Classroom)
 admin.site.register(Post)
 admin.site.register(Comment)
--- Code/Backend/classroom/classes/models.py
+++ Code/Backend/classroom/classes/models.py
@@ -5,6 +5,12 @@ from accounts.models import Faculty
 import uuid
 import random
 import string
+
+class ClassTime (models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
 class Classroom (models.Model):
     faculty=models.ForeignKey(User,on_delete=models.CASCADE,default=None)
     # facultyiD=models.OneToOneField(Faculty, on_delete= models.CASCADE)
@@ -17,8 +23,8 @@ class Classroom (models.Model):
     course_subtitle=models.CharField(max_length=20,null=True)
     course_description=models.CharField(max_length=500,null=True)
     course_section = models.CharField(max_length=3,null=True)
-
     class_code = models.CharField(max_length=6, null=True, blank=True, unique=True)
+    class_time = models.ForeignKey(ClassTime,null=True,on_delete= models.SET_NULL)
 
     def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
         return ''.join(random.choice(chars) for _ in range(size))
@@ -30,6 +36,9 @@ class Classroom (models.Model):
             while Classroom.objects.filter(urlhash=self.class_codes).exists():
                 self.class_codes = self.id_generator()
         super(Classroom, self).save()
+    
+    def __str__(self) -> str:
+        return self.course_title + ' Section ' +self.course_section
     # class Meta:
     #     db_table="Classes"
     # def __str__(self):
